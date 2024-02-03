a623b3043fdd9631d232a673e5d6a892240be3dd
Tomasz Pawlikowski <tp4458@gmail.com>
2024-02-03 23:59:31 +0000

    binary search iterative function

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+import inquirer from 'inquirer';
 
-function binarySearch(array, value) {
-  low_point = 0;
-  hi_point = array.length - 1;
+let array = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+
+inquirer
+  .prompt([{ type: 'input', message: 'select number 1-20', name: 'number' }])
+  .then((response) => binarySearchIterative(array, response));
+
+function binarySearchIterative(array, value) {
+  let val = Number(value.number);
+  console.log(val);
+  let low_point = 0;
+  let hi_point = array.length - 1;
+  console.log(low_point);
+  console.log(hi_point);
+  const startTimer = performance.now();
   while (low_point <= hi_point) {
-    mid = (low_point + hi_point) / 2;
-    if (array[mid] == value) {
-      return mid;
-    } else if (array[mid] < value) {
+    let mid = Math.floor((low_point + hi_point) / 2);
+    // console.log(mid);
+    if (array[mid] === val) {
+      console.log(`value found at index position ${mid}`);
+      const finishTimer = performance.now();
+      console.log(
+        `It took me ${
+          finishTimer - startTimer
+        } milliseconds complete the search`
+      );
+      return true;
+    } else if (array[mid] < val) {
       low_point = mid + 1;
     } else {
       hi_point = mid - 1;
     }
   }
-  return -1;
+  console.log('-1');
+
+  return false;
 }
