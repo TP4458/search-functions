85ecd0751f6a2b65bf4cfc4df717aae8a8aa2633
Tomasz Pawlikowski <tp4458@gmail.com>
2024-02-04 01:08:02 +0000

    added recursive search and inquirer prompt to select search type

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 import inquirer from 'inquirer';
 
-let array = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-];
+const array = Array(10000000)
+  .fill(0)
+  .map((_, index) => index);
 
 inquirer
-  .prompt([{ type: 'input', message: 'select number 1-20', name: 'number' }])
-  .then((response) => binarySearchIterative(array, response));
+  .prompt([
+    {
+      name: 'list',
+      type: 'list',
+      message: 'Select the type of function to use for index search:',
+      choices: [
+        'Binary search, iterative',
+        'Binary search, recursive',
+        'Linear search',
+      ],
+    },
+    {
+      type: 'input',
+      message: 'select number 1-10000000',
+      name: 'number',
+    },
+  ])
+  .then((response) => {
+    if ((response.list = 'Binary search, iterative')) {
+      console.log(response.number);
+      binarySearchIterative(array, response.number);
+    } else if ((response.list = 'Binary search, recursive')) {
+      console.log(response.number);
+      binarySearchRecursive(array, response.number);
+    }
+  });
 
 function binarySearchIterative(array, value) {
-  let val = Number(value.number);
+  console.log(value);
+  let val = Number(value);
   console.log(val);
   let low_point = 0;
   let hi_point = array.length - 1;
-  console.log(low_point);
-  console.log(hi_point);
   const startTimer = performance.now();
   while (low_point <= hi_point) {
     let mid = Math.floor((low_point + hi_point) / 2);
-    // console.log(mid);
     if (array[mid] === val) {
       console.log(`value found at index position ${mid}`);
       const finishTimer = performance.now();
@@ -34,7 +56,28 @@ function binarySearchIterative(array, value) {
       hi_point = mid - 1;
     }
   }
-  console.log('-1');
+  console.log('Number not found in the array');
 
   return false;
+}
+
+function binarySearchRecursive(array, value) {
+  let val = Number(value);
+  let low_point = 0;
+  let hi_point = array.length - 1;
+  const startTimer = performance.now();
+  if (low_point > hi_point) {
+    console.log('Number not found in the array');
+    return false;
+  }
+  let mid = Math.floor((low_point + hi_point) / 2);
+  if (array[mid] === val) {
+    const finishTimer = performance.now();
+    console.log(
+      `It took me ${finishTimer - startTimer} milliseconds complete the search`
+    );
+    return true;
+  }
+  if (array[mid] > x) return recursiveFunction(array, val, low_point, mid - 1);
+  else return recursiveFunction(array, val, mid + 1, hi_point);
 }
